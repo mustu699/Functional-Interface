73317557c107e401377cc14b6bf886b7ee1338a6
mustu699 <42728404+mustu699@users.noreply.github.com>
2021-02-11 20:44:14 +0530

    Add files via upload
    Creating a new Functional Interface in Java

---
+++ Calculate.java
@@ -0,0 +1,5 @@
+package com.example.java;
+@FunctionalInterface
+public interface Calculate {
+    int calculate(int a,int b);
+}
---
+++ Lambda_New.java
@@ -0,0 +1,16 @@
+package com.example.java;
+
+public class Lambda_New {
+
+    public static void main(String args[]) {
+        Calculate add = (a, b) -> a + b;
+        Calculate substract = (a, b) -> a - b;
+        Calculate multiply = (a, b) -> a * b;
+        Calculate divide = (a, b) -> (b != 0 ? a / b : 0);
+
+        System.out.println("Add Result: "+add.calculate(4,3));
+        System.out.println("Multiply Result: "+multiply.calculate(4,5));
+        System.out.println("Substract Result: "+substract.calculate(7,6));
+        System.out.println("Divide Result: "+divide.calculate(9,3));
+    }
+}
