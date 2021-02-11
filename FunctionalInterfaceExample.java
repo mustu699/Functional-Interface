21de44457cab93826322b8018cdcce5de5e07854
mustu699 <42728404+mustu699@users.noreply.github.com>
2021-02-11 16:49:30 +0530

    Add files via upload

---
+++ FunctionalInterfaceExample.java
@@ -0,0 +1,26 @@
+package com.example.java;
+
+import java.util.function.*;
+
+public class FunctionalInterfaceExample {
+    public static void main(String[] args) {
+        Predicate<String> stringPredicate = (s) -> s.length() < 10;
+        System.out.println(stringPredicate.test("Apple") + " Predicate example.It takes one argument and returns true or false");
+
+        Consumer<String> stringConsumer = (s) -> System.out.println(s.toLowerCase() + " Consumer example.It takes argument perform operation and return void ");
+        stringConsumer.accept("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+        Function<Integer, String> integerStringFunction = (num) -> Integer.toString(num);
+        System.out.println("Function Example .It takes argument type and return type both. " + integerStringFunction.apply(26));
+
+        Supplier<Double> doubleSupplier = () -> Math.random();
+        System.out.println(doubleSupplier.get() + " Supplier takes nothing in argument ,supplies return value");
+
+        UnaryOperator<String> unaryOperator = (a) -> a.toUpperCase();
+        System.out.println("Takes one argument " + unaryOperator.apply("Apple"));
+
+        BinaryOperator<Integer> binaryOperator = (a, b) -> a + b;
+        System.out.println("Takes 2 argument " + binaryOperator.apply(10, 20));
+
+    }
+}
